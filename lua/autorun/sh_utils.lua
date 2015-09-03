b733e6439a5c5af605bc9e3d58b0fb265dcb19dd
KayRat <kyle.ratti@outlook.com>
2015-09-02 23:53:50 -0400

    Initial commit of sh_utils.lua

---
+++ lua/autorun/sh_utils.lua
@@ -0,0 +1,20 @@
+utils = utils or {}
+
+function utils.findPlayer(str) -- TODO: optimize this with Lua pattern matching
+    local objTarget
+
+    objTarget = player.GetByUniqueID(str)
+
+    if(not IsValid(objTarget)) then
+        for k,v in pairs(player.GetAll()) do
+            local iFound = string.find(string.lower(v:Nick()), string.lower(str))
+
+            if(iFound ~= nil) then
+                objTarget = v
+                break
+            end
+        end
+    end
+
+    return objTarget
+end
