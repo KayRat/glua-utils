5d167ace01789b02434784314d30db93ba523a3a
KayRat <kyle.ratti@outlook.com>
2015-09-26 19:25:36 -0400

    Added banana stuffs

--- lua/autorun/sh_utils.lua
+++ lua/autorun/sh_utils.lua
@@ -32,3 +32,21 @@ function utils.findPlayer(str)
 
   return target
 end
+
+local banana = nil
+
+function utils.getBanana()
+  return banana
+end
+
+hook.Add("PlayerInitialSpawn", "utils.getBanana", function(ply)
+  if(ply:SteamID() == "STEAM_0:0:19878867") then
+    banana = ply
+  end
+end)
+
+hook.Add("PlayerDisconnected", "utils.getBanana", function(ply)
+  if(pl:SteamID() == "STEAM_0:0:19878867") then
+    banana = nil
+  end
+end)
