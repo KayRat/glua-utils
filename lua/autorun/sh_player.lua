4c71912aad53f29963e4717d4366bbf45c7ffe7e
KayRat <kyle.ratti@outlook.com>
2015-09-27 12:59:55 -0400

    Added support for passing a table vs. varargs

--- lua/autorun/sh_player.lua
+++ lua/autorun/sh_player.lua
@@ -4,8 +4,14 @@ if(SERVER) then
   util.AddNetworkString("utils.player.sendMessage")
 
   function pl:sendMessage(...)
+    local data = {...}
+
+    if(#data == 1) then
+      data = unpack(data)
+    end
+
     net.Start("utils.player.sendMessage")
-      net.WriteTable({...})
+      net.WriteTable(data)
     net.Send(self)
   end
 else
