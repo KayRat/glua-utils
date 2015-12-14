7f547cb3712215c4e60c77b5af2780c28b85cf3f
Kyle <kyle.ratti@outlook.com>
2015-12-13 23:20:20 -0500

    Fixed SRCDS preventing random map command

--- lua/autorun/server/sv_serverid.lua
+++ lua/autorun/server/sv_serverid.lua
@@ -60,7 +60,7 @@ local tblIDs = {
 
           local strMap = table.Random(tblFiles)
 
-          RunConsoleCommand("changelevel", string.sub(strMap, 0, string.len(strMap)-4))
+          game.ConsoleCommand("cmd changelevel", string.sub(strMap, 0, string.len(strMap)-4).."\n")
         end
       end
 
